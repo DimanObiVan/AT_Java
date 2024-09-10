09b3882d0ef37bf937ad733d1970031fe6979c16
DKKuznetsov <cyiiepmehdiman@gmail.com>
2024-09-10 21:21:40 +0300

    доработал скролл и шаги

--- src/main/java/helpers/Scroller.java
+++ src/main/java/helpers/Scroller.java
@@ -8,68 +8,54 @@ import pages.YandexAfterSearch;
 import static java.lang.Thread.sleep;
 
 public class Scroller {
-//    private static WebDriver chrome = new ChromeDriver();
-private static WebDriverWait wait;
 
 
     public static void scroll(String xPath, WebDriver chromeDriver) throws InterruptedException {
        WebElement scroller = chromeDriver.findElement(By.xpath(xPath));
-       //scroller.click();
         long startTime = System.currentTimeMillis();
-        long timeout = 35000;
+        long timeout = 45000;
        while (System.currentTimeMillis() - startTime < timeout) {
 
        //        chromeDriver.findElement(By.xpath(xPath)).click();
                scroller.sendKeys(Keys.PAGE_DOWN);
 
-           sleep(500);
-        //   WebElement footer = chromeDriver.findElement(By.xpath("//footer"));
+           sleep(700);
 
        }
     }
-    public static void scrollWindow(String xPath, String xPath2, WebDriver chromeDriver) throws InterruptedException {
-        WebElement scroller = chromeDriver.findElement(By.xpath(xPath));
-        scroller.click();
-        long startTime = System.currentTimeMillis();
-        long timeout = 15000;
-        WebElement element = chromeDriver.findElement(By.xpath(xPath2));
-        while (System.currentTimeMillis() - startTime < timeout) {
-            if(!element.isDisplayed()) {
-                System.out.println("Скроллер: не нашел элемент, начинаю скролл");
-                scroller.sendKeys(Keys.ARROW_DOWN);
-//                sleep(500);
-            } else break;
-            //   WebElement footer = chromeDriver.findElement(By.xpath("//footer"));
 
+    public static void scrollWithJS(WebDriver chromedriver) throws InterruptedException {
+        sleep(2000);
+        JavascriptExecutor js = (JavascriptExecutor) chromedriver;
+        WebElement element = chromedriver.findElement(By.xpath("//*[text()='Статистика']"));
+        while (true) {try {
+          boolean a = (boolean) js.executeScript(
+                    "var rect = arguments[0].getBoundingClientRect();" +
+                            "return (rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth));",
+                    element
+            );
+            // Проверяем, видим ли элемент
+            if (a) {
+                break;
+            }
+
+            // Скроллим страницу вниз
+            js.executeScript("window.scrollBy(0, 1000);");
+
+            // Ждем некоторое время, чтобы контент успел подгрузиться
+            Thread.sleep(1250);
+
+            // Проверяем снова
+            if (a) {
+                break;
+            }
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Thread was interrupted", e);
+        }
         }
     }
 
-//    public static WebElement scrollUntilElementIsFound(String xpath, WebDriver chromeDriver) throws InterruptedException {
-//        WebElement element = null;
-//        int scrollCount = 0;
-//
-//        while (scrollCount < 10) {
-//            try {
-//                // Попробуем найти элемент
-//                element = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
-//                if (element.isDisplayed()) {
-//                    System.out.println("Элемент найден на странице.");
-//                    return element;
-//                }
-//            } catch (NoSuchElementException | TimeoutException e) {
-//                // Игнорируем, если элемент не найден, и продолжаем прокрутку
-//            }
-//
-//            // Скроллим вниз на определенное количество пикселей
-//            ((JavascriptExecutor) chromeDriver).executeScript("window.scrollBy(0, window.innerHeight);");
-//            scrollCount++;
-//            Thread.sleep(1000); // Ожидание после прокрутки
-//
-//            System.out.println("Прокрутка " + scrollCount + " из " + 10);
-//        }
-//
-//        System.out.println("Элемент не найден после максимального числа прокруток.");
-//        return element;
-//    }
 
 }
